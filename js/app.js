3b19c2cd7821707efd92ae4e1d8d284a6f168d80
T Hunold <codeposse@gmail.com>
2014-08-14 00:07:57 -0700

    url

--- js/app.js
+++ js/app.js
@@ -25,7 +25,7 @@ MIH.FEDTest = {
   },
 
   DATA: {
-    search: "https://api.github.com/search/repositories/"
+    search: "https://api.github.com/search/repositories?q="
   },
 
   cache: (function(ss) {
